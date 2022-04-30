81cbf8053bfbbd620a9057fbb6a36195b699bcbf
Andrew McInally <realfunnyestate@yahoo.com>
2022-04-30 13:44:33 -0600

    more work on the app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 function App() {
+  const title = 'blog post'
+  const body = 'this is my blog post'
+  const comments = [
+    { id: 1, text: 'comment one' },
+    { id: 2, text: 'comment two' },
+    { id: 3, text: 'comment three' },
+  ]
+
   return (
     <div className="container">
-      <h1>My app</h1>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+      <div className="comments">
+        <h3>Comments({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+      
     </div>
   )
 }
