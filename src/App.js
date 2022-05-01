28950cac197d8e2d70c05fe41a24ddd84384b473
Andrew McInally <realfunnyestate@yahoo.com>
2022-04-30 18:07:17 -0600

    added a component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Header from './components/Header'
+
 function App() {
   return (
-    <div className="container">
-     <h1>My App</h1>
-    </div>
+    <>
+      <Header text={true}/>
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
   )
 }
+
+
 
 export default App
