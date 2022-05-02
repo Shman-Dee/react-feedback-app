28f944e98c81623d4a9e4f17fd3150bdb784e774
Andrew McInally <realfunnyestate@yahoo.com>
2022-05-01 21:24:50 -0600

    global styling and inline styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,29 @@
 import PropTypes from 'prop-types'
 
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  }
   return (
-    <div>
+    <header style={headerStyles}>
       <div className="container">
         <h2>{text}</h2>
       </div>
-    </div>
+    </header>
   )
 }
 
 Header.defaultProps = {
+  bgColor: 'rgba(0,0,0,0.5)',
+  textColor: '#ff6aaa',
   text: 'Feedback UI',
 }
 
 Header.propTypes = {
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
   text: PropTypes.string,
 }
 
