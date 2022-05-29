91312bd67ab3a5ba2738cc54b48e8c32944ddb34
Andrew McInally <realfunnyestate@yahoo.com>
2022-05-29 13:57:16 +0200

    more context hooks

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
@@ -6,15 +7,38 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'this item is from context',
+      text: 'this is feedback item 1',
+      rating: 10,
+    },
+    {
+      id: 2,
+      text: 'this is feedback item 2',
+      rating: 10,
+    },
+    {
+      id: 3,
+      text: 'this is feedback item 3',
       rating: 10,
     },
   ])
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
 
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        deleteFeedback,
+        addFeedback,
       }}
     >
       {children}
